e96877d5a9f25b3116bea3a5b8187924fa426dc0
Alex Makarov <alexpg27@yandex.ru>
2021-03-10 11:27:56 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 120;
+        int bonusMiles = ticketPrice / 20;
+        System.out.println("Вам начислено: " + bonusMiles + " миль");
+
+    }
+}
